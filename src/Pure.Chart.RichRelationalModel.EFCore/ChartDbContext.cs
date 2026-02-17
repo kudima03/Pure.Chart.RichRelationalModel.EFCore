b995f17c5333417033750aff5a5f32bf5826bbfd
Dmitry Kurochkin <kudima03@gmail.com>
2026-02-17 11:32:36 +0300

    ChartDbContext implemented

---
+++ src/Pure.Chart.RichRelationalModel.EFCore/ChartDbContext.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Pure.Chart.RichRelationalModel.EFCore.Models;
+using Pure.Chart.RichRelationalModel.EFCore.Models.Configurations;
+
+namespace Pure.Chart.RichRelationalModel.EFCore;
+
+public sealed class ChartDbContext(DbContextOptions<ChartDbContext> options)
+    : DbContext(options)
+{
+    public DbSet<ChartEFCoreModel> Diagrams => Set<ChartEFCoreModel>();
+
+    public DbSet<ChartTypeEFCoreModel> Types => Set<ChartTypeEFCoreModel>();
+
+    public DbSet<AxisEFCoreModel> Axes => Set<AxisEFCoreModel>();
+
+    public DbSet<SeriesEFCoreModel> Series => Set<SeriesEFCoreModel>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        _ = modelBuilder.ApplyConfiguration(new SeriesConfiguration());
+        _ = modelBuilder.ApplyConfiguration(new SeriesConfiguration());
+        _ = modelBuilder.ApplyConfiguration(new ChartTypeConfiguration());
+        _ = modelBuilder.ApplyConfiguration(new ChartConfiguration());
+    }
+}
